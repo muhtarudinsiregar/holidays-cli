8367435f81a63adcd6edc3662b5c354509356d2c
muhtarudinsiregar <muhtarudin@javan.co.id>
2016-12-27 21:43:16 +0700

    get date holidays

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+
+const baseUrl = "http://www.liburnasional.com/kalender-2017/";
+
+var holidays = [];
+
+axios.get(baseUrl).then(res => {
+    var $ = cheerio.load(res.data);
+    // get datemont
+    // return [ { date: '1 Januari 2017', ... }],
+    $(".libnas-calendar-holiday-datemonth").each(function(x, y) {
+        holidays.push({
+            date: y.children[0].data
+        });
+        return holidays;
+    });
+});
